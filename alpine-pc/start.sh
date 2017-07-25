c3db1a8a898de2094b784fbdba1030c7bd1dfa6e
Nicolas Alscher <nicolas.alscher@ihneo.fr>
2017-07-25 17:16:16 +0200

    PC project

---
+++ alpine-pc/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Retrieve app from S3
+cd ${APP_ROOT}
+aws s3api get-object --bucket ${AWS_S3_BUCKET} --key ${AWS_S3_KEY} app.zip
+
+# Unzip app
+unzip app.zip
+
+# Remove Zip file
+rm app.zip
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
